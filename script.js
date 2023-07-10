64471fceb7e988fdb8da8688faff1073ff1f6bea
Matthew-Chandler <personal@matthewachandler.com>
2023-07-09 23:27:24 -0700

    it works with buttons!

--- script.js
+++ script.js
@@ -1,57 +1,79 @@
-// Parse the HTML and extract food truck information
-function parseFoodTruckInfo(html) {
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html, "text/html");
+$(document).ready(function() {
+    $('#searchAllButton').click(function() {
+      getFoodTruckSchedule(null);
+      updateScheduleHeading("Schedule");
+    });
   
-    const lunchFoodTrucks = extractFoodTrucks(doc, "hours-open Lunch");
-    const dinnerFoodTrucks = extractFoodTrucks(doc, "hours-open Dinner");
-    const extendedDinnerFoodTrucks = extractFoodTrucks(doc, "hours-open Extended Dinner");
+    getFoodTruckList();
+  });
   
-    displayFoodTrucks("Lunch", lunchFoodTrucks);
-    displayFoodTrucks("Dinner", dinnerFoodTrucks);
-    displayFoodTrucks("Extended Dinner", extendedDinnerFoodTrucks);
-  }
+  function getFoodTruckList() {
+    var url = 'https://historical-reliable-fennel.glitch.me/';
   
-  // Extract food truck information for a specific mealtime
-  function extractFoodTrucks(doc, classSelector) {
-    const foodTruckCell = doc.querySelector(`td.${classSelector}`);
-    const foodTruckSpans = foodTruckCell.querySelectorAll("span.hours-additonal");
-  
-    const foodTrucks = Array.from(foodTruckSpans).map(span => span.textContent);
-    return foodTrucks;
-  }
-  
-  // Display food trucks for a specific mealtime on the HTML table
-  function displayFoodTrucks(mealtime, foodTrucks) {
-    const table = document.getElementById("foodTrucksTable");
-  
-    const row = document.createElement("tr");
-  
-    const mealtimeCell = document.createElement("td");
-    mealtimeCell.textContent = mealtime;
-  
-    const foodTrucksCell = document.createElement("td");
-    foodTrucksCell.textContent = foodTrucks.join(", ");
-  
-    row.appendChild(mealtimeCell);
-    row.appendChild(foodTrucksCell);
-  
-    table.appendChild(row);
-  }
-  
-  // Fetch the HTML content from a URL and parse it
-  function fetchAndParseHTML(url) {
-    fetch(url)
-      .then(response => response.text())
-      .then(html => {
-        parseFoodTruckInfo(html);
-      })
-      .catch(error => {
-        console.error("Error fetching HTML:", error);
+    $.get(url, function(data) {
+      var foodTrucks = [];
+      $(data).find('.hours-additonal').each(function() {
+        var foodTruck = $(this).text().trim();
+        if (!foodTrucks.includes(foodTruck)) {
+          foodTrucks.push(foodTruck);
+        }
       });
+  
+      var foodTruckButtons = $('#foodTruckButtons');
+      foodTruckButtons.find('button').remove(); // Clear previous buttons
+  
+      $.each(foodTrucks, function(index, foodTruck) {
+        var button = $('<button></button>').text(foodTruck);
+        button.click(function() {
+          getFoodTruckSchedule(foodTruck);
+          updateScheduleHeading(foodTruck);
+        });
+        foodTruckButtons.append(button);
+      });
+    });
   }
   
-  // Usage example
-  const url = "https://www.ucladining.com"; // Replace with the desired URL
-  fetchAndParseHTML(url);
+  function getFoodTruckSchedule(foodTruckName) {
+    var url = 'https://historical-reliable-fennel.glitch.me/';
+  
+    $.get(url, function(data) {
+      var scheduleTable = $('#scheduleTable');
+      scheduleTable.find('tr:gt(0)').remove(); // Clear previous search results
+  
+      $(data).find('tr').each(function() {
+        var location = $(this).find('.hours-location').text().trim();
+        var periods = [];
+  
+        $(this).find('.hours-open').each(function() {
+          var period = $(this).attr('class').split(' ')[1];
+          var foodTrucks = $(this).find('.hours-additonal').map(function() {
+            return $(this).text().trim();
+          }).get();
+  
+          if (!foodTruckName || foodTrucks.includes(foodTruckName)) {
+            periods.push(getFullPeriodName(period));
+          }
+        });
+  
+        if (periods.length > 0) {
+          var row = $('<tr></tr>');
+          $('<td></td>').text(location).appendTo(row);
+          $('<td></td>').text(periods.join(', ')).appendTo(row);
+          scheduleTable.append(row);
+        }
+      });
+    });
+  }
+  
+  function getFullPeriodName(period) {
+    if (period === 'Extended') {
+      return 'Extended Dinner';
+    }
+    return period;
+  }
+  
+  function updateScheduleHeading(headingText) {
+    var scheduleHeading = $('#scheduleHeading');
+    scheduleHeading.text(headingText);
+  }
   
